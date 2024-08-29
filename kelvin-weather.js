731a266f768a29d8500b892ee372b0a4c5d9f5ad
Dani <174685774+itsonlydaniiii@users.noreply.github.com>
2024-08-29 15:16:38 +0200

    Fix bug - add ,

---
+++ kelvin-weather.js
@@ -0,0 +1,21 @@
+// Today's Kelvin forecast
+const kelvin = '293';
+// Convert Kelvin to Celsius
+console.log(293 - 273);
+const celcius = `${kelvin} - 273`;
+// Kelvin to Fahrenheit conversion
+let fahrenheit = `${celcius} * (9/5) + 32`;
+// Round down Fahrenheit temperature
+fahrenheit = Math.floor($,{fahrenheit});
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+    
+  
+  
+  
+  
+  
+  
+
+  
+
+  
